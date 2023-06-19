29ad552450a7f2965808230eb75f37e8fafc96b2
sebasvisser <sebas.visser@mensenrelatie.nl>
2023-06-19 21:02:00 +0200

    class pokemon trainer aangemaakt als eerste stap

---
+++ src/PokemonTrainer.java
@@ -0,0 +1,11 @@
+public class PokemonTrainer {
+    private String name;
+    private List<Pokemon> pokemons;
+
+    public PokemonTrainer(String name, List<Pokemon> pokemons) {
+        this.name = name;
+        this.pokemons = pokemons;
+    }
+
+    // other methods...
+}
