0d718ad55f8a1fdb3fc4f8ed3aafd5c0efc92858
sebasvisser <sebas.visser@mensenrelatie.nl>
2023-06-20 00:21:58 +0200

    met hulp van intelliJ methode extracted en variabele final gemaakt

--- src/ElectricPokemon.java
+++ src/ElectricPokemon.java
@@ -2,11 +2,11 @@ import java.util.List;
 import java.util.Objects;
 
 public class ElectricPokemon implements Pokemon {
-    private String name;
+    private final String name;
     private int hp;
     private int level;
-    private String food;
-    private String sound;
+    private final String food;
+    private final String sound;
 
     public ElectricPokemon(String name, int hp, int level, String food, String sound) {
         this.name = name;
@@ -57,12 +57,14 @@ public class ElectricPokemon implements Pokemon {
     }
 
     public void thunderPunch(Pokemon pokemon, Pokemon gymPokemon) {
-        String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
         System.out.println(pokemon.getName()
                 + " attacks "
                 + gymPokemon.getName()
-                + " with "
-                + methodName);
+                + " with Thunder Punch");
+        electricAttack(gymPokemon);
+    }
+
+    private void electricAttack(Pokemon gymPokemon) {
         if(Objects.equals(gymPokemon.getType(), "Water")){
             System.out.println("It's Super Effective!");
             System.out.println(gymPokemon.getName()
@@ -105,146 +107,26 @@ public class ElectricPokemon implements Pokemon {
     }
 
     public void electroBall(Pokemon pokemon, Pokemon gymPokemon) {
-        String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
         System.out.println(pokemon.getName()
                 + " attacks "
                 + gymPokemon.getName()
-                + " with "
-                + methodName);
-        if(Objects.equals(gymPokemon.getType(), "Water")){
-            System.out.println("It's Super Effective!");
-            System.out.println(gymPokemon.getName()
-                    + " loses 20 hp.");
-            gymPokemon.setHp(20);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
-        if(Objects.equals(gymPokemon.getType(), "Grass")){
-            System.out.println("It's Effective!");
-            System.out.println(gymPokemon.getName()
-                    + " loses 15 hp.");
-            gymPokemon.setHp(15);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
-        if(Objects.equals(gymPokemon.getType(), "Fire")){
-            System.out.println(gymPokemon.getName()
-                    + " loses 10 hp.");
-            gymPokemon.setHp(10);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
-        if(Objects.equals(gymPokemon.getType(), "Electric")){
-            System.out.println("It's Not Very Effective!");
-            System.out.println(gymPokemon.getName()
-                    + " loses 5 hp.");
-            gymPokemon.setHp(5);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
+                + " with Electro Ball");
+        electricAttack(gymPokemon);
     }
 
     public void thunder(Pokemon pokemon, Pokemon gymPokemon) {
-        String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
         System.out.println(pokemon.getName()
                 + " attacks "
                 + gymPokemon.getName()
-                + " with "
-                + methodName);
-        if(Objects.equals(gymPokemon.getType(), "Water")){
-            System.out.println("It's Super Effective!");
-            System.out.println(gymPokemon.getName()
-                    + " loses 20 hp.");
-            gymPokemon.setHp(20);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
-        if(Objects.equals(gymPokemon.getType(), "Grass")){
-            System.out.println("It's Effective!");
-            System.out.println(gymPokemon.getName()
-                    + " loses 15 hp.");
-            gymPokemon.setHp(15);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
-        if(Objects.equals(gymPokemon.getType(), "Fire")){
-            System.out.println(gymPokemon.getName()
-                    + " loses 10 hp.");
-            gymPokemon.setHp(10);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
-        if(Objects.equals(gymPokemon.getType(), "Electric")){
-            System.out.println("It's Not Very Effective!");
-            System.out.println(gymPokemon.getName()
-                    + " loses 5 hp.");
-            gymPokemon.setHp(5);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
+                + " with Thunder");
+        electricAttack(gymPokemon);
     }
 
     public void voltTackle(Pokemon pokemon, Pokemon gymPokemon) {
-        String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
         System.out.println(pokemon.getName()
                 + " attacks "
                 + gymPokemon.getName()
-                + " with "
-                + methodName);
-        if(Objects.equals(gymPokemon.getType(), "Water")){
-            System.out.println("It's Super Effective!");
-            System.out.println(gymPokemon.getName()
-                    + " loses 20 hp.");
-            gymPokemon.setHp(20);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
-        if(Objects.equals(gymPokemon.getType(), "Grass")){
-            System.out.println("It's Effective!");
-            System.out.println(gymPokemon.getName()
-                    + " loses 15 hp.");
-            gymPokemon.setHp(15);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
-        if(Objects.equals(gymPokemon.getType(), "Fire")){
-            System.out.println(gymPokemon.getName()
-                    + " loses 10 hp.");
-            gymPokemon.setHp(10);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
-        if(Objects.equals(gymPokemon.getType(), "Electric")){
-            System.out.println("It's Not Very Effective!");
-            System.out.println(gymPokemon.getName()
-                    + " loses 5 hp.");
-            gymPokemon.setHp(5);
-            System.out.println(gymPokemon.getName()
-                    + " has "
-                    + gymPokemon.getHp()
-                    + " hp remaining.");
-        }
+                + " with Volt Tackle");
+        electricAttack(gymPokemon);
     }
 }
